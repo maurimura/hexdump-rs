7cd7569697f858af5736df7aaade745ac28abfa6
Mauricio Muratori <maurimura@gmail.com>
2021-08-29 13:56:56 -0300

    Read from file and prints the output

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+const BYTES_PER_LINE: usize = 16;
+
+fn main() {
+    let arg1 = env::args().nth(1);
+    let fname = arg1.expect("usage: hexdump-rs FILENAME");
+
+    let mut f = File::open(&fname).expect("Unable to open file.");
+    let mut pos = 0;
+    let mut buffer = [0; BYTES_PER_LINE];
+
+    while let Ok(_) = f.read_exact(&mut buffer) {
+        print!("[0x{:09x}] ", pos);
+        for byte in &buffer {
+            match *byte {
+                0x00 => print!(". "),
+                0xff => print!("## "),
+                _ => print!("{:02x} ", byte),
+            }
+        }
+
+        println!("");
+        pos += BYTES_PER_LINE;
+    }
+}
